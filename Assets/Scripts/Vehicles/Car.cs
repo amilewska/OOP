2023e8e3270937f449b1e03ea4eff5d7ba8f9ab1
amilewska <adoryanna@gmail.com>
2023-03-06 17:12:56 +0100

    vehicle

--- Assets/Scripts/Vehicles/Car.cs
+++ Assets/Scripts/Vehicles/Car.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class Car : Vehicle
 {
-   
+    
+
+    protected override void Start()
+    {
+        base.Start();
+        speed = 10;
+        speedRotation = 10;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
 }
